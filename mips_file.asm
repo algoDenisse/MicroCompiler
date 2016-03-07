74c0dec2d1ae8b725f1a0623937fd6812534de6b
algodenisse <denissepaolarojas@hotmail.com>
2016-03-07 02:02:01 +0000

    Pruebas 1

--- mips_file.asm
+++ mips_file.asm
@@ -1,38 +1,20 @@
 .data
     A: .word 0 
-    BB: .word 0 
-    CC: .word 0 
+    B: .word 0 
+    B: .word 0 
     Temp1: .word 0 
-    Temp2: .word 0 
-    DD: .word 0 
-    Temp3: .word 0 
 .text
     main:
-    li $v0, 5
-    syscall
-    sw $v0, BB
-    li $v0, 5
-    syscall
-    sw $v0, CC
-    li $v0, 5
-    syscall
-    sw $v0, DD
-    lw $a0, BB
-    lw $a1, CC
+    li $a0, 2
+    sw $a0, A
+    li $a0, 3
+    sw $a0, B
+    lw $a0, A
+    lw $a1, B
     add $v0, $a0, $a1
     sw $v0, Temp1
-    lw $a0, Temp1
-    lw $a1, A
-    add $v0, $a0, $a1
-    sw $v0, Temp2
-    lw $a0, Temp2
-    lw $a1, DD
-    add $v0, $a0, $a1
-    sw $v0, Temp3
-    lw $a0, Temp3
-    sw $a0, A
     li $v0, 1
-    lw $a0, A 
+    lw $a0, Temp1 
     syscall 
     la $v0, 10
     syscall
